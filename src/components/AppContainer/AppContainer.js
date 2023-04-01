2853792633345aa07eb5b00b13e0387642934679
dhrgic <dhrgic@gmail.com>
2023-04-02 00:42:17 +0200

    Initial commit

---
+++ src/components/AppContainer/AppContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const AppContainer = (props) => {
+  return <div>AppContainer</div>;
+};
+
+AppContainer.propTypes = {};
+
+export default AppContainer;
---
+++ src/components/InputBox/InputBox.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const InputBox = (props) => {
+  return <div>InputBox</div>;
+};
+
+InputBox.propTypes = {};
+
+export default InputBox;
